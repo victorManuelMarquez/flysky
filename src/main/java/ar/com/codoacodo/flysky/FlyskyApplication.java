c836283e9b11ef8ab2d595d344e932f58d398c34
victor <victormmla3lm@gmail.com>
2023-11-29 19:40:30 -0300

    Initial commit

---
+++ src/main/java/ar/com/codoacodo/flysky/FlyskyApplication.java
@@ -0,0 +1,13 @@
+package ar.com.codoacodo.flysky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyskyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlyskyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/com/codoacodo/flysky/FlyskyApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.codoacodo.flysky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyskyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
