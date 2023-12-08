8c2ce911cbf614187c54a24d9df59250e7969428
Fede <santiagok-po2010@hotmail.com>
2023-12-08 12:27:34 -0300

    fix

--- src/main/java/ar/com/codoacodo/flysky/controller/AsientoController.java
+++ src/main/java/ar/com/codoacodo/flysky/controller/AsientoController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequestMapping("/api/asientos")
 public class AsientoController {
     private IAsientoService service;
 
